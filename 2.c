d14ed937f6f1e20db2e678b58246a366471b1e7a
518h0144 <74368879+518h0144@users.noreply.github.com>
2021-06-19 13:54:54 +0700

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,10 +1,42 @@
+/*
+    cau 2
+*/
 #include<stdio.h>
+#define N 100
+
+void input(int *arr, int *n);
+void output(int *arr,int n);
+void doubleAnArray(int *arr,int n);
+
 int main(){
-    int a , b , c;
-    printf("Nhap cac so A, B, C :");
-    scanf("%d%d%d",&a,&b,&c);
-    if(a > b && a >c) printf("A la so lon nhat");
-    else if (b > a && b > c) printf("B la so lon nhat");
-    else printf("C la so lon nhat");
+    int n;
+    printf("Enter N of Array:");
+    scanf("%d",&n);
+    int arr[N];
+    input(arr,&n);
+    doubleAnArray(arr,n);
+    printf("\nDoubel first 3 Element in array : \n");
+    output(arr,n);
     return 0;
+}
+void input(int *arr, int *n){
+    while((*n)<=0){
+        printf("Enter N (N > 0) : ");
+        scanf("%d",n);
+    }
+    int i;
+    for(i=0;i<(*n);i++){
+        printf("arr[%d] = ",i);
+        scanf("%d",&arr[i]);
+    }
+}
+void doubleAnArray(int *arr,int n){
+    for(int i=0;i<3;i++){
+        arr[i] = arr[i] * 2;
+    } 
+}
+void output(int *arr,int n){
+    for(int i=0;i<n;i++){
+        printf("%-10d",arr[i]);
+    }
 }
